71b1cb46eac13201041f17496e16f87ab4120650
WilliamBerlin76 <william.berlin76@gmail.com>
2019-10-12 19:41:10 -0500

    header looks better, added component files, using sass for styling

--- src/Components/header.js
+++ src/Components/header.js
@@ -2,19 +2,29 @@ import React from 'react';
 import styled from 'styled-components';
 
 const HeaderWrapper = styled.div`
+    margin: 0 auto;
     color: rgb(199, 199, 199);
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
     max-width: 800px;
+    height: 65vh;
 `;
 const HeaderLayout = props => {
     return(
         <HeaderWrapper key = {props.id}>
-            <img src={props.imgUrl}></img>
-            <div>
-                <h1>William Berlin</h1>
-                <p>Web Developer</p>
+            <div className='header-info-container'>
+                <img className="profile-pic"src={props.imgUrl}/>
+                <div className="name-links">
+                    <h1 className="name">William Berlin</h1>
+                    <p className="job-title">Web Developer</p>
+                    <a href="https://github.com/WilliamBerlin76">
+                        <i className="fab fa-github"/>
+                    </a>
+                    <a href="https://www.linkedin.com/in/william-berlin-75695b193/">
+                        <i class="fab fa-linkedin"/>
+                    </a>
+                </div>
             </div>
         </HeaderWrapper>
     );
---
+++ src/dataArrays.js
@@ -0,0 +1,8 @@
+export const projectData = [
+    {name:'', link:''},
+    {name:'', link:''},
+    {name:'', link:''},
+    {name:'', link:''},
+    {name:'', link:''},
+    {name:'', link:''}
+]
