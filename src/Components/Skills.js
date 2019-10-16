3e106298ec3c8eaeaab771fe18a5488f57c96ba0
WilliamBerlin76 <william.berlin76@gmail.com>
2019-10-16 16:24:58 -0500

    skills section basic build

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -4,13 +4,31 @@ import React from 'react';
 const Skills = () => {
 
     return(
-        <>
-            <h1>Skills</h1>
-            <i className="fab fa-html5"></i>
-            <i className="fab fa-css3-alt"></i>
-            <i className="fab fa-js-square"></i>
-            <i className="fab fa-react"></i>
-        </>
+        <section className='skills-container'>
+            <h2>Skills</h2>
+            <div className='skills-list'>
+                <h3 className='skill-card-title'> HTML5 
+                    <p>
+                        <i className="fab fa-html5"></i>
+                    </p>
+                </h3>
+                <h3 className='skill-card-title'> CSS/LESS/SASS
+                    <p>
+                        <i className="fab fa-css3-alt"></i>
+                    </p>
+                </h3>
+                <h3 className='skill-card-title'> JavaScript
+                    <p>
+                        <i className="fab fa-js-square"></i>
+                    </p>
+                </h3>
+                <h3 className='skill-card-title'> React.js/Redux
+                    <p>
+                        <i className="fab fa-react"></i>
+                    </p>    
+                </h3>
+            </div>
+        </section>
     )
 }
 
