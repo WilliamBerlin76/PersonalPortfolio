c9661158611409a9d7fd52e7dababd22028a794b
WilliamBerlin76 <william.berlin76@gmail.com>
2019-12-08 18:34:13 -0600

    started styling form, state now being set on form input changes

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,22 +1,38 @@
 import React, { useState } from 'react';
 
 const Contact = () => {
-    const [message, setMessage] = useState({})
+    const [message, setMessage] = useState({});
+    
+    const handleChanges = e => {
+        setMessage({
+            ...message,
+            [e.target.name]: e.target.value
+        })
+    };
+    
     return (
-        <section className='contact-form'>
+        <section className='contact-form-container'>
             <h2>Contact Me!</h2>
-            <form>
+            <form classname='contact form'>
                 <input
-                    placeholder='Name'    
+                    placeholder='Name'  
+                    name='senderName' 
+                    onChange={handleChanges} 
                 />
                 <input
                     placeholder='Email'
+                    name='senderEmail'
+                    onChange={handleChanges}
                 />
                 <input
                     placeholder='Subject'
+                    name='subject'
+                    onChange={handleChanges}
                 />
                 <textarea
                     placeholder='Message'
+                    name='message'
+                    onChange={handleChanges}
                 />
                 <button>Send</button>
             </form>
