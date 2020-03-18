f1dbbea97746ba2c04113e543769d0aef6830d87
WilliamBerlin76 <william.berlin76@gmail.com>
2020-03-17 21:18:49 -0500

    added ant game to projects, cleaned up data array

--- src/dataArrays.js
+++ src/dataArrays.js
@@ -1,10 +1,36 @@
 export const projectData = [
-    {name:'mNeme (flashcards application)', role: 'Full Stack Web Developer', frontend:'https://github.com/Lambda-School-Labs/flashcards-fe', backend:'https://github.com/Lambda-School-Labs/flashcards-be', deployedLink: 'https://mneme-staging.netlify.com/'},
-    {name:'Weight Lifting Journal', role: 'Back End Web Developer', backend:'https://github.com/Build-Week-Weight-Lifiting-Journal/Back-end', deployedLink: 'https://weight-lifiting-journal.netlify.com/'},
-    {name:'Guidr', role:'Front End React/state management & authentication', githubLink: 'https://github.com/Build-Week-FT-Guidr', deployedLink:'https://guidr.now.sh/'},
-    {name:'Essentialism', role:'Front End React', githubLink:'https://github.com/Build-Week-Essentialism-2/Essentialism-FE', deployedLink: 'https://app-essentialism.netlify.com/'},
-    {name:'Expat Journal', role:'Landing Page UI', githubLink:'https://github.com/ExpatJournal/Marketing-Page-William-Berlin', deployedLink: 'https://pensive-euler-ed7f27.netlify.com/'},
+    {
+        name:'mNeme (flashcards application)', 
+        role: 'Full Stack Web Developer', 
+        frontend:'https://github.com/Lambda-School-Labs/flashcards-fe', 
+        backend:'https://github.com/Lambda-School-Labs/flashcards-be', 
+        deployedLink: 'https://mneme-staging.netlify.com/'
+    },
+    {
+        name:'Ant Adventure', 
+        role:'Front End React', 
+        githubLink:'https://github.com/CS26-Team-PleaseComeToBrazil-Mud-Game', 
+        deployedLink:'https://ant-adventure.now.sh/'
+    },
+    {
+        name:'Weight Lifting Journal', 
+        role: 'Back End Web Developer', 
+        backend:'https://github.com/Build-Week-Weight-Lifiting-Journal/Back-end',
+        deployedLink: 'https://weight-lifiting-journal.netlify.com/'
+    },
+    {
+        name:'Guidr', 
+        role:'Front End React', 
+        githubLink: 'https://github.com/Build-Week-FT-Guidr', 
+        deployedLink:'https://guidr.now.sh/'
+    },
+    {
+        name:'Essentialism', 
+        role:'Front End React', 
+        githubLink:'https://github.com/Build-Week-Essentialism-2/Essentialism-FE', 
+        deployedLink: 'https://app-essentialism.netlify.com/'
+    },
+    // {name:'Expat Journal', role:'Landing Page UI', githubLink:'https://github.com/ExpatJournal/Marketing-Page-William-Berlin', deployedLink: 'https://pensive-euler-ed7f27.netlify.com/'},
     // {name:'Naruto Filler List', role:'Solo project - React/Redux', githubLink:'https://github.com/WilliamBerlin76/React-Redux-App', deployedLink:'https://react-redux-app-49lkdn9dv.now.sh/'},
-    // {name:'', githubLink:''},
     // {name:'', githubLink:''}
 ]
