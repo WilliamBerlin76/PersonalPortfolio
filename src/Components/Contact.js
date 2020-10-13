0d283f434fdab43f1dd6d2de086aab58d0958224
WilliamBerlin76 <william.berlin76@gmail.com>
2020-10-13 15:24:04 -0500

    added more precise form validation

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -6,17 +6,40 @@ import axios from 'axios';
 const Contact = () => {
     const [message, setMessage] = useState({});
     const [isFetching, setIsFetching] = useState(false);
+    const [nameErr, setNameErr] = useState(false);
+    const [emailErr, setEmailErr] = useState(false);
+    const [subjectErr, setSubjectErr] = useState(false);
+    const [messageErr, setMessageErr] = useState(false);
+
+    const emailReg = new RegExp(/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/);
     
     const handleChanges = e => {
         setMessage({
             ...message,
             [e.target.name]: e.target.value
         })
+        message.senderName && setNameErr(false);
+        message.subject && setSubjectErr(false);
+        message.message && setMessageErr(false);
+        message.senderEmail && emailReg.test(message.senderEmail) && setEmailErr(false);
     };
-    const sendMessage = () => {
-        if (!message.senderName || !message.senderEmail || !message.subject || !message.message ) {
-            alert('Please fill in all form fields')
-        } else {
+    const sendMessage = e => {
+        if (!message.senderName){
+            setNameErr(true);
+        } 
+        if (!message.senderEmail || !emailReg.test(message.senderEmail)){
+            setEmailErr(true);
+        } 
+
+        if (!message.subject){
+            setSubjectErr(true);
+        } 
+
+        if (!message.message){
+            setMessageErr(true);
+        } 
+        
+        if (message.senderEmail && emailReg.test(message.senderEmail) && message.senderName && message.subject && message.message) {
             setIsFetching(true);
             axios.post(process.env.REACT_APP_API_POST, message)
             .then(res => {
@@ -28,7 +51,9 @@ const Contact = () => {
                 alert('There was an error sending your message to the server');
                 setIsFetching(false);
             });
-        }  
+        } else {
+            e.preventDefault();
+        }
     };
     
     return (
@@ -36,18 +61,24 @@ const Contact = () => {
             <h2>Contact Me!</h2>
             <form className='contact-form'>
                 <div className='small-buttons-container'>
-                    <input
-                        className='small-input'
-                        placeholder='Name'  
-                        name='senderName' 
-                        onChange={handleChanges} 
-                    />
-                    <input
-                        className='small-input'
-                        placeholder='Email'
-                        name='senderEmail'
-                        onChange={handleChanges}
-                    />
+                    <div className="input-err-container">
+                        <input
+                            className='small-input'
+                            placeholder='Name'  
+                            name='senderName' 
+                            onChange={handleChanges} 
+                        />
+                        {nameErr && (<p className="contact-errors">*Please enter your name</p>)}
+                    </div>
+                    <div className="input-err-container">
+                        <input
+                            className='small-input'
+                            placeholder='Email'
+                            name='senderEmail'
+                            onChange={handleChanges}
+                        />
+                        {emailErr && <p className="contact-errors">*Please enter a valid email</p>}
+                    </div>
                 </div>
                 <input
                     className='form-subject'
@@ -55,13 +86,14 @@ const Contact = () => {
                     name='subject'
                     onChange={handleChanges}
                 />
+                {subjectErr && <p className="contact-errors">*Please add a subject</p>}
                 <textarea
                     className='form-message'
                     placeholder='Message'
                     name='message'
                     onChange={handleChanges}
                 />
-                
+                {messageErr && <p className="contact-errors">*Please add a message</p>}
                 { isFetching === true ? 
                     <div className='sending-message'>
                         <ClipLoader 
